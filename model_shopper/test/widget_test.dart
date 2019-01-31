6a28e82a5c0a0f45b8f59800cc8cf265756ffdaa
Filip Hracek <filip.hracek@gmail.com>
2019-01-31 13:36:02 -0800

    Fix the smoke test

--- model_shopper/test/widget_test.dart
+++ model_shopper/test/widget_test.dart
@@ -5,26 +5,31 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:model_shopper/main.dart';
+import 'package:model_shopper/models/cart.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('app smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(
+      ScopedModel<CartModel>(
+        model: CartModel(),
+        child: MyApp(),
+      ),
+    );
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify that our app starts at the login screen.
+    expect(find.text('Welcome'), findsOneWidget);
+    expect(find.text('Catalog'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // Tap 'Enter'.
+    await tester.tap(find.text('ENTER').first);
+    await tester.pumpAndSettle();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that we changed screens.
+    expect(find.text('Welcome'), findsNothing);
+    expect(find.text('Catalog'), findsOneWidget);
   });
 }
