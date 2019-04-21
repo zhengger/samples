3d8b765ec1208d22666c4573f84a15ca57e12a85
Filip Hracek <filiph@users.noreply.github.com>
2019-04-21 14:49:49 -0700

    Doc update

--- model_shopper/lib/models/cart.dart
+++ model_shopper/lib/models/cart.dart
@@ -10,7 +10,7 @@ class CartModel extends Model {
   /// An unmodifiable view of the items in the cart.
   UnmodifiableListView<Item> get items => UnmodifiableListView(_items);
 
-  /// The current total price of all items (assuming all items cost $1).
+  /// The current total price of all items (assuming all items cost $42).
   int get totalPrice => _items.length * 42;
 
   /// Adds [item] to cart. This is the only way to modify the cart from outside.
